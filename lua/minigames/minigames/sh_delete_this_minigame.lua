18650171209a752079654d9102d80d02576e6fa6
Pythagorion <43889730+Pythagorion@users.noreply.github.com>
2021-11-19 18:12:12 +0100

    added other files

---
+++ lua/minigames/minigames/sh_delete_this_minigame.lua
@@ -0,0 +1,2 @@
+//this minigame just exists to build the basic folder structure up. If you added the first minigame, please delete this file since it has no functionality
+// -- Pytho
---
+++ lua/terrortown/gamemode/shared/hud_elements/test_hud_element.lua
@@ -0,0 +1,2 @@
+//this hud element just exists to build the basic folder structure up. If you added the first hud element, please delete this file since it has no functionality
+// -- Pytho
